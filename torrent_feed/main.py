26e44cc896f2df8c3778cef6f45a3febff4713ca
int3l <int3l@users.noreply.github.com>
2020-09-15 00:16:07 +0300

    xbtit_feed program skeleton

---
+++ torrent_feed/main.py
@@ -0,0 +1,19 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='xbtit_feed')
+    parser.add_argument(
+        '-V',
+        '--version',
+        action='version',
+        version='%(prog)s 0.0.1',
+    )
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    try:
+        exit(main())
+    except KeyboardInterrupt:
+        raise SystemExit(1)
