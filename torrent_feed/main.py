fdcb99ab97a48eec7fe07e7ab93d2b62c39290ec
int3l <int3l@users.noreply.github.com>
2020-10-03 02:12:00 +0300

    Refactoring for the configuration loading
    and addition option for output control

--- torrent_feed/main.py
+++ torrent_feed/main.py
@@ -1,81 +1,93 @@
-from collections import deque
 import argparse
+import json
 import re
+from collections import deque
+from functools import lru_cache
+from time import sleep
 
 import feedparser
 
 
-DEQUE_LIMIT = 100
-PARSER_CONFIG_FILENAME = 'config.json'
+CONFIG_FILE_PATH = 'config.json'
 TITLE_PATTERN = re.compile(r'\[.*?\] (?P<title>.*?) \[SEEDERS.*\]')
 TORRENT_URL_PATTERN = re.compile(
-    r'^(?P<href>http://(.*)/download\.php\?id=[a-z0-9]{30,40}\&'
-    r'f=[a-zA-Z0-9%.-]{1,600}\.torrent&rsspid=[a-z0-9]{30,40})$'
+    r'^(?P<href>http://.*/download\.php\?id=[a-z0-9]{30,40}\&'
+    r'f=[a-zA-Z0-9%.-]{1,600}\.torrent&rsspid=[a-z0-9]{30,40})$',
 )
 
 
+@lru_cache(maxsize=1)
+def load_config(filename=CONFIG_FILE_PATH):
+    try:
+        with open(filename) as json_config_file:
+            return json.load(json_config_file)
+    except FileNotFoundError:
+        print('Configuration file missing')
+        raise SystemExit(1)
+    except json.decoder.JSONDecodeError:
+        print('Configuration file decoding error')
+        raise SystemExit(1)
+
+
+def deque_limit():
+    return load_config().get('deque_limit')
+
+
+def parser_config():
+    return load_config().get('parser')
+
+
 def extract_title(title):
-    title = title.encode('utf-8')
     title_match = TITLE_PATTERN.search(title)
     if title_match:
         return title_match.group('title').strip()
-
-    print('Bad title {}'.format(title))
-
+    else:
+        print(f'Bad title {title}')
 
 
 def extract_url(enclosures):
     if not enclosures or enclosures[0].type != 'application/x-bittorrent':
-        print('Bad enclosures {}'.format(enclosures))
+        print(f'Bad enclosures {enclosures}')
         return
 
     url_match = TORRENT_URL_PATTERN.search(enclosures[0].href)
     if url_match:
         return url_match.group('href')
+    else:
+        print(f'Bad URL {enclosures[0].href}')
 
-    print('Bad URL {}'.format(enclosures[0].href))
 
-
-def tracker(parser_config):
-    rss = feedparser.parse(**parser_config)
+def tracker():
+    rss = feedparser.parse(**parser_config())
     if rss.status != 200 or rss.bozo != 0:
-        print('Bad status {}, bozo {}'.format(rss.status, rss.bozo))
-        print('Bad {}'.format(rss.bozo_exception))
+        print(f'Bad status {rss.status}, bozo {rss.bozo}')
+        print(f'Bad {rss.bozo_exception}')
         return
 
     for entry in rss.entries:
         yield (
             entry.id,
             extract_title(entry.title),
-            extract_url(entry.enclosures)
+            extract_url(entry.enclosures),
         )
 
 
 def rss_feed(args):
-    last_torrents = deque((tid for tid, _, _ in extractor()), DEQUE_LIMIT)
-    parser_config = load_json(PARSER_CONFIG_FILENAME)
+    last_torrents = deque((tid for tid, _, _ in tracker()), deque_limit())
 
     while True:
         sleep(5)
-        for tid, title, url in tracker(parser_config):
+        for tid, title, url in tracker():
             if tid in last_torrents:
                 continue
 
             last_torrents.append(tid)
             if not title or not url:
-                print('Bad \n{}\n{}\n{}\n'.format(tid, title, url))
                 continue
-
-            yield title, url
-
-
-def load_json(filename):
-    try:
-        with open(filename) as json_config_file:
-            return json.load(json_config_file)
-    except Exception:
-        print('Configuration file missing/error')
-        raise SystemExit(1)
+            elif args.url:
+                print(f'{title}\n{url}')
+            else:
+                print(title)
 
 
 def main():
@@ -86,6 +98,7 @@ def main():
         action='version',
         version='%(prog)s 0.0.1',
     )
+    parser.add_argument('-u', '--url', action='store_true')
     args = parser.parse_args()
     rss_feed(args)
 
